d418b087ed1256468041266164c28ec89e3c24c0
Kris Bredemeier <34@holbertonschool.com>
2016-02-06 14:00:31 -0800

    hexidecimal in order

--- i_m_not_in_the_loop/3-print_base16.c
+++ i_m_not_in_the_loop/3-print_base16.c
@@ -1,9 +1,14 @@
 #include "my_functions.h"
 void print_base16(void)
 {
-	char c;
-	for (c='0'; c<='F'; ++c)
+	char x;
+	char y;
+	for (x='0' ; x<='9' ; x++)
 	{
-	print_char(c);
+	print_char(x);
+	}
+	for(y='A' ; y<='F' ; y++)
+	{
+	print_char(y);
 	}
 }
