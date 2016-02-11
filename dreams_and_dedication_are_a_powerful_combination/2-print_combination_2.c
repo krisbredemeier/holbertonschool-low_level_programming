c55b9bbf8ea16d4e1cb12431107425a75703992d
Kris Bredemeier <34@holbertonschool.com>
2016-02-11 14:41:05 -0800

    removes additional comma

--- dreams_and_dedication_are_a_powerful_combination/2-print_combination_2.c
+++ dreams_and_dedication_are_a_powerful_combination/2-print_combination_2.c
@@ -14,8 +14,11 @@ if (x>n)
 	{
 	print_char(n+'0');
 	print_char(x+'0');
-if (n!=89)
-		{	
+if ((n==8) && (x==9))
+{
+
+}
+	else	{	
 		print_char(',');
 		print_char(' ');
 		}
--- dreams_and_dedication_are_a_powerful_combination/3-print_combination_3.c
+++ dreams_and_dedication_are_a_powerful_combination/3-print_combination_3.c
@@ -1,9 +1,6 @@
 /*Prints three digit number combinations with no repeats*/
-
 #include "my_functions.h"
-
 void print_combination_3(void)
-
 {
 int n;
 int x;
@@ -17,8 +14,11 @@ if ((y>x) && (x>n))
 	print_char(n+'0');
 	print_char(x+'0');
 	print_char(y+'0');
-if (n!=789)
-		{	
+if ((n==7) && (x==8) && (y==9))
+{
+
+}
+	else	{	
 		print_char(',');
 		print_char(' ');
 		}
