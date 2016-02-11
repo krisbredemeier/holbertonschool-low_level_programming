78ce9b768697d39354697712f9461a634aab0a69
Kris Bredemeier <34@holbertonschool.com>
2016-02-11 10:05:17 -0800

    add file commas and spaces

---
+++ dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
@@ -0,0 +1,22 @@
+/*
+*this function prints nubmer 0 throgh 9
+*with a comma and space to seperate
+*/
+
+#include "my_functions.h"
+
+void print_combination_1(void)
+
+/*selcts numbers 0 thorugh 9*/
+{
+int n;
+for (n=0; n<=9; n++)
+	{print_char(n+'0');
+	if (n!=9)/*applies below loop to all numbers besides 9*/
+		{
+		print_char(',');
+		print_char(' ');
+		}
+	}
+}
+
