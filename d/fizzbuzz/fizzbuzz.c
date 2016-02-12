c5f5a7d9afe24677ae778036697925eef933f2fe
Kris Bredemeier <34@holbertonschool.com>
2016-02-12 13:41:42 -0800

    no repedtitive spaces

--- d/fizzbuzz/fizzbuzz.c
+++ d/fizzbuzz/fizzbuzz.c
@@ -1,3 +1,4 @@
+/*this is a comment for fizzbuzz*/
 #include <stdio.h>
 
 int main()
@@ -5,16 +6,20 @@ int main()
 	int i;
 	for (i = 1; i < 101; i++)
 	{
-		printf("%d ", i);
 		if (i % 15 == 0){
-			printf("fizzbuzz ");
+			printf("fizzbuzz");
 		} 
 		else if (i % 5 == 0) {
-			printf("buzz ");
+			printf("buzz");
 		}
 		else if (i % 3 == 0) {
-			printf("fizz ");
+			printf("fizz");
 		}
+		else printf("%d", i);
+		if (i<100)
+			{
+			printf(" ");
+			}
 	}
 }
 
