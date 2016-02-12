103771e7d1008fffc4ac0c1dae548bedf999acaf
Kris Bredemeier <34@holbertonschool.com>
2016-02-12 11:06:25 -0800

    adds numbers between fizz and buzz

--- d/fizzbuzz/fizzbuzz.c
+++ d/fizzbuzz/fizzbuzz.c
@@ -5,14 +5,15 @@ int main()
 	int i;
 	for (i = 1; i < 101; i++)
 	{
+		printf("%d ", i);
 		if (i % 15 == 0){
-			printf("%d fizz-buzz ", i);
+			printf("fizz-buzz ");
 		} 
 		else if (i % 5 == 0) {
-			printf("%d buzz ", i);
+			printf("buzz ");
 		}
 		else if (i % 3 == 0) {
-			printf("%d fizz ", i);
+			printf("fizz ");
 		}
 	}
 }
