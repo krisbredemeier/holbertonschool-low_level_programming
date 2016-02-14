d7bda528b3d303d8ebb6c069e3cc611b1d87542d
Kris Bredemeier <34@holbertonschool.com>
2016-02-14 11:50:22 -0800

    cleans up file and removes excess lines

--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -1,10 +1,6 @@
 #include "my_functions.h"
-/*
-*this file writes a function that
-*takes an integer in parameter and prints it
-*/
+/*this file writes a function that takes an integer in parameter and prints it*/
 void print_number(int n)
-/*this is a list of decalred variables*/
 {
 int digits;
 int temp;
@@ -12,41 +8,24 @@ int power;
 int singledigit;
 char chardigit;
 int digitscopy;
-/*
-*the loop will not work if the nubmer is 0,
-*so there must be a seperate if statment for this case
-*/
-if ( n == 0 )
-	{
+/* this takes care of 0 and negative numbers*/
+if ( n == 0 ) {
 	print_char('0');
-	return;
-	}
-/*
-*if the number is less then 0, you need to make it a positive
-*to to run the loop
-*then add back in the minus symbol
-*/
-if ( n < 0 )
-	{
+	return; }
+if ( n < 0 ) {
 	print_char ('-');
-	n = n * -1;
-	}
+	n = n * -1; }
 /*figure out number of digits in the varaible*/
 temp = n;
 digits =0;
-while (temp > 0)
-	{
+while (temp > 0) {
 	temp = temp / 10;
-	digits++;
-	}
+	digits++; }
 digitscopy = digits; /*store the value into copy*/
-for (power = 1; digits > 1; digits--)
-	{
-	power = power * 10; 
-	}
+for (power = 1; digits > 1; digits--) {
+	power = power * 10; }
 /*start loop */
-for (; digitscopy > 0; digitscopy--)
-	{
+for (; digitscopy > 0; digitscopy--) {
 	singledigit = n /power; /*divide n by power */
 	chardigit = singledigit + '0'; /*print the digit */
 	print_char(chardigit);
