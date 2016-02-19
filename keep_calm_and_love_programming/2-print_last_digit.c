f9a0736d461a132f5fe004820c1da7bb0e5fea68
Kris Bredemeier <34@holbertonschool.com>
2016-02-18 16:53:50 -0800

    i love c

--- keep_calm_and_love_programming/2-print_last_digit.c
+++ keep_calm_and_love_programming/2-print_last_digit.c
@@ -8,7 +8,7 @@ return (write(1, &c, 1));
 void print_last_digit(int n)
 {
 int length;
-int expo=1;
+int power=1;
 int number=n;
 if (n<0) {
 number = number*-1;
@@ -18,9 +18,9 @@ for(length=0; number > 0; length++) {
 number=number/10;
 }
 while(length>1) {
-expo=expo*10;
+power=power*10;
 length--;
 }
-n=n%expo;
+n=n%power;
 print_char(n+'0');
 }
