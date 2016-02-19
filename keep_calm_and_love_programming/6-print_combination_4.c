72f6392babdf92a98030caeb8bdb794add90d1c3
Kris Bredemeier <34@holbertonschool.com>
2016-02-19 10:50:45 -0800

    i love c

--- keep_calm_and_love_programming/6-print_combination_4.c
+++ keep_calm_and_love_programming/6-print_combination_4.c
@@ -1,38 +1,26 @@
 /*this prints all possible combinations of two two differnt digits*/
+
 #include "my_functions.h"
-void print_combination_2(void)
-
+#include <stdio.h>
+void print_combination_4(void)
 {
-int w;
-int x;
-int y;
-int z;
-int A;
-int B;
-	for (w=0; w<= 9; w++)
+int a;
+int b;	
+	for (a=0; a< 99; a++)
 	{
-	for (x=0; x<= 9; x++)
-	{
-	for (y=0; y<=9; y++)
-	{
-	for (z=1; z<=9; z++)
+		for (b= a+1; b<=99; b++)
+		{		
+		print_char ((a / 10) + '0');
+		print_char ((a % 10) + '0');
+		print_char (' ');
+		print_char ((b / 10) + '0');
+		print_char ((b % 10) + '0');
 
-		A= w*10+x;
-		B= y*10+z;
-		if(A<B)
-			{
-			print_char(w+'0');
-			print_char(x+'0');
-			print_char(' ');
-			print_char(y+'0');
-			print_char(z+'0');
-		if (A!=98)
-			{
-			print_char(',');
-			print_char(' ');
-			}
+		if (a!=98)
+					{
+					print_char(',');
+					print_char(' ');
+					}
 		}
 	}
-}
-}
 }
