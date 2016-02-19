be5597848aa83ae0e360d7fb4b7e954a4685d46d
Kris Bredemeier <34@holbertonschool.com>
2016-02-18 17:04:54 -0800

    adds fizzbuzz again

---
+++ keep_calm_and_love_programming/fizzbuzz/fizzbuzz.c
@@ -0,0 +1,24 @@
+/*this is my first fizzbuzzz project*/
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 15 == 0){
+			printf("FizzBuzz");
+		} 
+		else if (i % 5 == 0) {
+			printf("Buzz");
+		}
+		else if (i % 3 == 0) {
+			printf("Fizz");
+		}
+		else printf("%d", i);
+		if (i<100)
+			{
+			printf(" ");
+			}
+	}
+}
