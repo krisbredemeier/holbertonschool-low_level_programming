c60bb27bfb95c28f2cd8486ec7537d319069b2b1
Kris Bredemeier <34@holbertonschool.com>
2016-02-20 14:02:29 -0800

    adds square

--- reefinery/print_square.c
+++ reefinery/print_square.c
@@ -2,40 +2,40 @@
 void print_square(int n)
 {
 int hyphen, pipe, space, stack, ncopy; 
-n = ncopy;
+ncopy = n;
 if (ncopy <= 0) 
 {}
 if (ncopy == 1){
-    print_char('o' +0);
+    print_char('o');
     print_char('\n');
     }
 if (ncopy == 2 ) {
-    print_char('o' + 0);
-    print_char('o' + 0);
+    print_char('o');
+    print_char('o');
     print_char('\n');
-    print_char('o' + 0);
-    print_char('o' + 0);
+    print_char('o');
+    print_char('o');
     print_char('\n');
     } 
     if (ncopy > 2) {
         print_char('o'); /*and n-2 hyphens*/
-        for (hyphen = ncopy-2; hyphen > 2; hyphen--) {
+        for (hyphen = ncopy; hyphen > 2; hyphen--) {
             print_char('-');
         }
             print_char('o');
             print_char('\n');
             {
-            for (stack = ncopy-2; stack > 2; stack--) {
-                print_char('|' + 0 );
-                for (space = ncopy-2; space > 2; space--) {
-                    print_char(' ' + 0);
+            for (stack = ncopy; stack > 2; stack--) {
+                print_char('|');
+                for (space = ncopy; space > 2; space--) {
+                    print_char(' ');
                 }
-                    print_char('|' + 0);
+                    print_char('|');
                     print_char('\n');
                 }
         if (ncopy > 2) {
         print_char('o'); /*and n-2 hyphens*/
-        for (hyphen = ncopy-2; hyphen > 2; hyphen--) {
+        for (hyphen = ncopy; hyphen > 2; hyphen--) {
             print_char('-');
         }
             print_char('o');
