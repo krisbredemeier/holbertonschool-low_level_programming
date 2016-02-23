53a4b4f7a94d8595793c54fd8fc14142f49ecf83
Kris Bredemeier <34@holbertonschool.com>
2016-02-23 10:10:36 -0800

    i love c soooo much

---
+++ its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
@@ -0,0 +1,16 @@
+/*prints the first digit of a number*/
+#include <unistd.h>
+#include "my_functions.h"
+int first_digit(int n)
+{
+int length;
+int number=n;
+	if(n>0) {
+	number=number*-1;
+	n=n*-1;
+	}
+	for(length=0; number<-9; length++) {
+		number=number/10;
+	}
+	return(-number);
+}
