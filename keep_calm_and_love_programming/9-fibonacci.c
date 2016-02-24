2e094a4ab62433ed810c31576f0a8671373a7549
Kris Bredemeier <krisbredemeier@gmail.com>
2016-02-24 18:33:31 +0000

    finanlly discovered lu

--- keep_calm_and_love_programming/9-fibonacci.c
+++ keep_calm_and_love_programming/9-fibonacci.c
@@ -4,18 +4,18 @@
 int main(void)
 {
 unsigned int fib1=1;
-unsigned int fib2=1;
-unsigned int fib3;
+unsigned long fib2=1;
+unsigned long fib3;
 int ndx = AMOUNT;
     {
-    printf("\n%d", fib2);
+    printf("\n%lu", fib2);
     while (ndx--)
         {
         fib3= fib2;
         fib2 += fib1;
         fib1 = fib3;
-        if (fib2 < 100){
-        printf(", %d", fib2);
+        if (fib3 <= 20365011074){
+        printf(", %lu", fib2);
             }
         }
     printf("\n");
