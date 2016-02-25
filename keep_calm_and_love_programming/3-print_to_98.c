960334cb26cda4ed816f54bca418a3994d2366af
Kris Bredemeier <krisbredemeier@gmail.com>
2016-02-25 15:46:05 +0000

    adds new file

--- keep_calm_and_love_programming/3-print_to_98.c
+++ keep_calm_and_love_programming/3-print_to_98.c
@@ -3,11 +3,11 @@
 void print_to_98(int n)
 {
 	while (n<98)
-	{ printf("%d ,",n);
+	{ printf("%d, ",n);
 	n++;
 	}
 	while (n>98)
-	{ printf("%d ,",n);
+	{ printf("%d, ",n);
 	n--;
 	}
 	printf("98\n");
