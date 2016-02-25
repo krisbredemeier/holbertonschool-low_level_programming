cc0baeb2fb9491840cc1e6c732d62bfea8af2d31
Kris Bredemeier <34@holbertonschool.com>
2016-02-25 09:36:18 -0800

    for looplooploop installed

--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -6,14 +6,17 @@ void triangles_in_the_term(int h, int n)
 	int i;//current row
 	int s; //spaces
 	int c; //counter (for *s)
+	int ncopy=n;
 	//n=nuber of times tree repeats
-	for(i=1;i<=h;i++)
-	{
-		for(s=0;s<h-i ;s++)
-			printf(" ");
-		for(c=0;c<(2*i-1) ;c++) 
-			printf("*");
-		printf("\n");
-	}
+
+		for(ncopy=0; ncopy<n; ++ncopy)
+			for(i=1;i<=h;i++)
+		{
+				for(s=0;s<h-i ;s++)
+					printf(" ");
+				for(c=0;c<(2*i-1) ;c++) 
+					printf("*");
+				printf("\n");
+			}
 		//repeate what is above 
 }
