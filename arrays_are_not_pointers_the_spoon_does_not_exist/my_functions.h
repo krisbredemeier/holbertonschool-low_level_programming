ab572b0db39fde850d0740bfacd933ab893f9713
Kris Bredemeier <34@holbertonschool.com>
2016-03-10 08:10:34 -0800

    adds print_number

--- arrays_are_not_pointers_the_spoon_does_not_exist/my_functions.h
+++ arrays_are_not_pointers_the_spoon_does_not_exist/my_functions.h
@@ -1,1 +1,22 @@
 int print_char(char c);
+
+void print_number(int n) {
+  int i = 1;
+  int temp;
+  int mod = 1;
+  if (n < 0) {
+    print_char('-');
+    mod = -1;
+  }
+  for(temp = n; temp > 9 || temp < -9; temp = temp / 10) {
+    i++;
+  }
+  while(i) {
+    int i2 = i--;
+    temp = n;
+    while(i2-- > 1) {
+      temp /= 10;
+    }
+    print_char(((temp % 10) * mod + '0'));
+  }
+}
