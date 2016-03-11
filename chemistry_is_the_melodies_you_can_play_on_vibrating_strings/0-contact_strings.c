eaeb3fbfcf69b201be90a2a477ab1cad78c2d677
Kris Bredemeier <34@holbertonschool.com>
2016-03-11 12:39:05 -0800

    concats a string

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/0-contact_strings.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/0-contact_strings.c
@@ -1,31 +1,12 @@
-#include "my_functions.h"
-
 //char *concat_strings(char *dest, const char *src)
-char *concat_strings(char *dest, const char *src)
-{
-  while (*dest) ++dest;
-  for (; *dest == *src; ++dest, ++src);
-
-
+char *concat_strings(char *dest, const char *src){
+ int i=0, j;
+ while (dest[i]!='\0'){
+   i++;
 }
-  /*
-char s1, s2;
-int i, j;
-for(i=0, dest[i]!='\0'; i++);
-
-
-for(j=0, src[j]!='\0'; j++)
-
-{
-  s1[i]=s2[j];
+ for (j = 0; src[j] != '\0'; i++, j++) {
+   dest[i] = src[j];
+ }
+ dest[i] = '\0';
+ return dest ;
 }
-s1[i]='\0';
-}*/
-
-
-/*{
-char *result= malloc(strlen(dest)+strlen(src)+1);
-strcpy(result, dest);
-strcat(result, src);
-return result;
-}*/
