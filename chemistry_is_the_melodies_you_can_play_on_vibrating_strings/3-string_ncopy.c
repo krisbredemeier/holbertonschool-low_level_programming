06031ae94dcfa4d2804382081eb3ccc5c08c8054
Kris Bredemeier <34@holbertonschool.com>
2016-03-11 14:05:04 -0800

    adds string ncopy

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
@@ -0,0 +1,15 @@
+char *string_ncopy(char *dest, const char *src, int n);
+char *
+string_ncopy(char *s1, const char *s2, int n)
+{
+    char *s = s1;
+    while (n > 0 && *s2 != '\0') {
+	*s++ = *s2++;
+	--n;
+    }
+    while (n > 0) {
+	*s++ = '\0';
+	--n;
+    }
+    return s1;
+}
