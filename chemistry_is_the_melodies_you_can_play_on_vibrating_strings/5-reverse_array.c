ea9b2b88b3f8b35b0c0ceae76e357516a70a37bf
Kris Bredemeier <34@holbertonschool.com>
2016-03-14 13:04:19 -0700

    first commit

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/5-reverse_array.c
@@ -0,0 +1,11 @@
+#include "my_functions.h"
+void reverse_array(int *a, int n)
+{
+  int i, temp;
+    for (i=0; i<n/2; i++)
+    {
+        temp = a[i];
+        a[i] = a[n-i-1];
+        a[n-i-1] = temp;
+    }
+}
