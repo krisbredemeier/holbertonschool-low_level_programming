de8fec8fb9ae085a0b37c4f88a16dda64ec599bb
Kris Bredemeier <34@holbertonschool.com>
2016-03-15 08:14:40 -0700

    prints line

---
+++ the_shortest_distance_between_two_points_is_a_straight_line/0-print_line.c
@@ -0,0 +1,23 @@
+/*prints line*/
+#include "my_functions.h"
+void print_line(int n)
+{
+  int ncopy = n;
+  int hyphen;
+  for (hyphen = ncopy; hyphen > 0; hyphen--) {
+      print_char('-'); }
+      print_char('\n'); {
+      }
+    }
+
+
+
+
+
+  /*
+for(int i=0; i<n; i++){
+  print_char('_');
+  print_char('\n');
+  }
+}
+*/
