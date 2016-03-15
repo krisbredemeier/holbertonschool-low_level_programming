75239612000661bd5c1a796fb15be35d90609f04
Kris Bredemeier <34@holbertonschool.com>
2016-03-15 16:15:35 -0700

    adds file

--- the_shortest_distance_between_two_points_is_a_straight_line/1-print_diagonal.c
+++ the_shortest_distance_between_two_points_is_a_straight_line/1-print_diagonal.c
@@ -3,11 +3,14 @@
 void print_diagonal(int n) {
 
 int i, j;
+if (n<=0) {
+  print_char('\n');
+}
 for (i=1;i<=n;i++) {
-  for (j=1; j<=i; j++)
+  for (j=1; j<=i-1; j++)
   print_char(' ');
   print_char('\\');
   print_char('\n');
   }
-  print_char('\n');
+
 }
