a8c8cb762adf647cf7640e35a9c6e50ab298bc95
Kris Bredemeier <34@holbertonschool.com>
2016-03-16 14:18:06 -0700

    makes first letter cap

--- if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/1-cap_string.c
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/1-cap_string.c
@@ -3,10 +3,11 @@
 char *cap_string(char *n)
   {
       int i;
-      for(i=0; i != 'E'; i++)
+      for(i=0; n[i] != '\0'; i++)
       {
-          if ((n[i-1]==' ' && n[i]>='a' && n[i]<='z'))
+          if ((n[0] >= 'a' && n[0] <= 'z') || (n[i-1]==' ' && n[i]>='a' && n[i]<='z'))
           n[i] = (n[i] -32);
+
       }
       return(n);
   }
