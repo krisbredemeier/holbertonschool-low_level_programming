3ddd37f65587e5f1465d42258d582e3316414648
Kris Bredemeier <34@holbertonschool.com>
2016-03-16 13:29:28 -0700

    changes to uppercase

---
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/0-uppercase_string.c
@@ -0,0 +1,10 @@
+/*changes all lowercase letters of a string to uppercase*/
+char *uppercase_string(char *n) {
+  int i =0;
+while(n[i] != '\0') {
+    if (n[i] >= 'a' && n[i] <= 'z')
+    n[i] = n[i] - 32;
+    i++;
+  }
+return(n);
+}
