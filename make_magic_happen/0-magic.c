2692bf3532b85ee5c955494c636bb11b88061afe
Kris Bredemeier <krisbredemeier@gmail.com>
2016-03-17 23:29:40 +0000

    remvoes excess characters

--- make_magic_happen/0-magic.c
+++ make_magic_happen/0-magic.c
@@ -8,11 +8,7 @@ int main(void) {
   a[2] = 1024;
   p = &n;
   *(p+5) = 98;
-  
-<<<<<<< HEAD
-
-=======
->>>>>>> 715f26a3aaebe5d54ec86e5117dabcd0792a0b72
+ 
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
