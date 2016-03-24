f0e36429d61be9d1cea720eb34ce88157edac31f
Kris Bredemeier <34@holbertonschool.com>
2016-03-24 10:40:04 -0700

    uses recursion

--- let_s_go_deeper/3-is_prime_number.c
+++ let_s_go_deeper/3-is_prime_number.c
@@ -1,13 +1,13 @@
 int is_prime_number(int n)
 {
-  int i;
-  if(n==1 || n==-1)
-  return 0;
-  for(i=2;i<n;i++){
-    if(n%i==0)
-    return 0;
-    if(n==1 || n==-1)
-    return 0;
-  }
+int i=2;
+if(n==1 || n==-1)
+return 0;
+    while(i<=n/2){
+         if(n%i==0)
+             return 0;
+         else
+             i++;
+    }
     return 1;
 }
