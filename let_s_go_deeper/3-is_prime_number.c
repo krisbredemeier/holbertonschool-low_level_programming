cafb10537f3dc88e69a032737cf95d2486d2d594
Kris Bredemeier <34@holbertonschool.com>
2016-03-25 13:33:04 -0700

    prints prime numbers

--- let_s_go_deeper/3-is_prime_number.c
+++ let_s_go_deeper/3-is_prime_number.c
@@ -1,13 +1,13 @@
+int prime(int n, int i)
+{
+  if(n <=1) return 0;
+  if(i > n/2) return 1;
+  if(n%i==0) return 0;
+  return prime(n, i+1);
+}
+
+
 int is_prime_number(int n)
 {
-int i=2;
-if(n==1 || n==-1)
-return 0;
-    while(i<=n/2){
-         if(n%i==0)
-             return 0;
-         else
-             i++;
-    }
-    return 1;
+  return prime(n, 2);
 }
