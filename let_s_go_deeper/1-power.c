66c11ceee34d6bfdf428c90d551d606995a85547
Kris Bredemeier <34@holbertonschool.com>
2016-03-25 13:02:37 -0700

    works on recursion

--- let_s_go_deeper/1-power.c
+++ let_s_go_deeper/1-power.c
@@ -1,12 +1,11 @@
 int power(int x, int y)
 {
-  int result = 1;
+  if(y==0)
+    return 1;
   if(y<0)
     return -1;
-  while(y)
-  {
-    result = result * x;
+  else {
     y--;
+    return power(x, y)*x;
   }
-  return result;
 }
