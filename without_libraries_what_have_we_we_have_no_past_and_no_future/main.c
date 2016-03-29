efa06ae502e4b39a28672474d27b08a4e7304a40
Kris Bredemeier <34@holbertonschool.com>
2016-03-28 18:29:43 -0700

    adds files to library

---
+++ without_libraries_what_have_we_we_have_no_past_and_no_future/main.c
@@ -0,0 +1,8 @@
+int power(int x,int y);
+
+int main(void)
+{
+int x;
+   x=power(1, 2);
+   return (0);
+}
---
+++ without_libraries_what_have_we_we_have_no_past_and_no_future/print_string.c
@@ -0,0 +1,7 @@
+
+/*this is not the right function*/
+/* prints absolute numbers*/
+int absolute(int v)
+{
+	return v * ( (v<0) * (-1) + (v>0));
+}
