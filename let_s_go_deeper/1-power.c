fa14778878beda89696769e1ee8af2441c438d83
Kris Bredemeier <34@holbertonschool.com>
2016-03-30 12:07:02 -0700

    commits changes

--- let_s_go_deeper/1-power.c
+++ let_s_go_deeper/1-power.c
@@ -4,8 +4,8 @@ int power(int x, int y)
     return 1;
   if(y<0)
     return -1;
-    y--;
+  y--;
   if((power(x, y)*x) > 2147483647)
     return -1;
-    return power(x, y)*x;
+  return power(x, y)*x;
 }
