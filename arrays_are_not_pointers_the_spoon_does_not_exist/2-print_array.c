80dc62a49766acfccadf60b4473ac57c8703f09b
Kris Bredemeier <krisbredemeier@gmail.com>
2016-03-31 16:32:13 +0000

    works in vm

--- arrays_are_not_pointers_the_spoon_does_not_exist/2-print_array.c
+++ arrays_are_not_pointers_the_spoon_does_not_exist/2-print_array.c
@@ -1,8 +1,10 @@
 /*function that prints each element of an array of integers.*/
 #include "my_functions.h"
+void print_number(int n);
 
 void print_array(int *a, int n) {
-  for (int i=0; i < n; i++) {
+  int i;
+  for (i=0; i < n; i++) {
     print_number(a[i]);
     if (i == (n-1)) {
       break;
