cdfd2abc1feaee802827611671c5a743969037d9
Kris Bredemeier <34@holbertonschool.com>
2016-04-26 12:59:34 -0700

    prints the alphabet backwards

---
+++ cisdoublefun_basics/1-print_tebahpla.c
@@ -0,0 +1,13 @@
+/*prints the alphabet backwards using ASCII*/
+int print_char(char c);
+
+void print_tebahpla(void)
+{
+  int i;
+  i=122;
+    while(i>=97)
+    {
+      print_char(i);
+      i--;
+    }
+}
