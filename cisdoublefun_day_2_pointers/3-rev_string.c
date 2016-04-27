97da95fc70a0d383fd5d2da3eab6b021afa7add7
Kris Bredemeier <34@holbertonschool.com>
2016-04-27 14:45:40 -0700

    reverses a string

---
+++ cisdoublefun_day_2_pointers/3-rev_string.c
@@ -0,0 +1,32 @@
+/*function reverses a string*/
+int string_length (char *str);
+
+char *rev_string(char *str)
+{
+  int i;
+  int j;
+  int temp;
+  i = 0;
+  j = string_length(str) -1;
+
+  while(i<j)
+  {
+    temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+    i++;
+    j--;
+  }
+return(str);
+}
+
+int string_length (char *str)
+{
+  int i;
+  i = 0;
+  while(str[i] != '\0')
+  {
+    i++;
+  }
+  return(i);
+}
