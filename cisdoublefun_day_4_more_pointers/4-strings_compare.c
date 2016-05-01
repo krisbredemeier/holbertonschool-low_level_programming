16cb3d2779d265b7da8d3e3c04ed6849b4c9f84e
Kris Bredemeier <34@holbertonschool.com>
2016-05-01 08:36:19 -0700

    cmapres strings

---
+++ cisdoublefun_day_4_more_pointers/4-strings_compare.c
@@ -0,0 +1,10 @@
+/*works like strcmp*/
+int strings_compare(char *s1, const char *s2)
+{
+  while(*s1 && (*s1==*s2))
+  {
+    s1++;
+    s2++;
+  }
+  return *(const unsigned char*)s1-*(const unsigned char*)s2;
+}
