13a11c059e8705dc9251a66b374a6d5e5efef564
Kris Bredemeier <krisbredemeier@gmail.com>
2016-05-02 04:54:37 +0000

    uses ncat to concat

---
+++ cisdoublefun_day_5_libraries_argc_argv/2-string_nconcat.c
@@ -0,0 +1,28 @@
+/*nconcat string*/
+
+char *string_nconcat(char *dest, const char *src, int n)
+{
+  char *temp;
+  int i;
+
+  temp = dest;
+  i = 0;
+  while(*dest)
+  {
+    dest++;
+  }
+  dest--;
+  if(*dest != '\n')
+  {
+    dest++;
+  }
+  while (i<n)
+  {
+    *dest=*src;
+    dest++;
+    src++;
+    i++;
+  }
+  *dest='\0';
+  return temp;
+}
