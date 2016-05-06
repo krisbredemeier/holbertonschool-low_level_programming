9148d158524d6c3b5ac982e8f437c990f7308028
Kris Bredemeier <34@holbertonschool.com>
2016-05-06 10:06:34 -0700

    works with macros and structs in c

---
+++ macros_and_structures/0-abs.h
@@ -0,0 +1,2 @@
+/* prints absolute numbers*/
+#define ABS(v) v * ((v<0) * (-1) + (v>0))
---
+++ macros_and_structures/2-print_string_struct.c
@@ -0,0 +1,57 @@
+ #include <unistd.h>
+/*function that prints the content of a struct String*/
+int print_char(char c);
+void print_number(int n);
+
+void print_string_struct(struct String *str)
+{
+  int i;
+
+  i = 0;
+  while (i < str->length)
+  {
+    print_char(str->str[i]);
+    i++;
+  }
+  print_char(',');
+  print_char(' ');
+  print_char(str->length);
+  print_char('\n');
+}
+
+/*prints a number*/
+
+void print_number(int n)
+{
+  int i;
+  int temp;
+  int x;
+
+  i = 1;
+  x = 1;
+  if (n < 0)
+  {
+    print_char('-');
+    x = -1;
+  }
+  for (temp = n; temp > 9 || temp < -9;temp = temp / 10)
+  {
+    i++;
+  }
+  while (i)
+  {
+    int i2;
+    i2 = i--;
+    temp = n;
+    while (i2-- > 1)
+    {
+      temp /= 10;
+    }
+    print_char(((temp % 10) * x + '0'));
+  }
+}
+
+int print_char(char c)
+    {
+          return (write(1, &c, 1));
+    }
