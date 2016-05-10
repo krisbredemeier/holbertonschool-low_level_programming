45735d631efb311df951e9019dca9a991a58fb39
Kris Bredemeier <34@holbertonschool.com>
2016-05-10 14:51:00 -0700

    corrects error in makefile

--- file_descriptors/0-print_content/0-print_content.c
+++ file_descriptors/0-print_content/0-print_content.c
@@ -1,5 +1,5 @@
-#include<fcntl.h>
-#include<stdio.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 int read(int handle, void *buffer, int nbyte);
 int write(int handle, void *buffer, int nbyte);
@@ -7,29 +7,40 @@ int close(int handle);
 
 int main(int argc, char *argv[])
 {
- int fileiopened;
- int whattoprint;
- char buf[BUFSIZ];
+  int fd;
+  int len;
+  char buf[BUFSIZ];
 
- /* if the program name and 1 argument are received
-  * try to open the file passed as argument
-  */
- if (argc == 2)
-   {
-     /* open, read, write, and close with error
-      * checking at each stage.
-      */
-     fileiopened = open(argv[1], O_RDONLY);
-     if (fileiopened == -1)
-       return 1;
-     whattoprint = read(fileiopened, buf, BUFSIZ);
-     if (whattoprint == -1)
-       return 1;
-     if ((write(1, buf, whattoprint)) == -1)
-       return 1;
-     if ((close(fileiopened)) == -1)
-       return 1;
-     return(0);
-   }
- return 1;
+  /* if the program name and 1 argument are received
+   * try to open the file passed as argument
+   */
+  if (argc == 2)
+    {
+      /* open, read, write, and close with error
+       * checking at each stage.
+       */
+      fd = open(argv[1], O_RDONLY);
+      if (fd == -1)
+        {
+          perror("open");
+          return 1;
+        }
+      len = read(fd, buf, BUFSIZ);
+      if (len == -1)
+        {
+          perror("read");
+          close(fd);
+        return 1;
+        }
+      if ((write(1, buf, len)) == -1)
+        {
+          perror("write");
+          close(fd);
+          return 1;
+        }
+      if ((close(fd)) == -1)
+        return 1;
+      return 0;
+    }
+  return 1;
 }
