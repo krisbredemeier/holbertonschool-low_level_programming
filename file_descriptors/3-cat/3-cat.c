7d0df22c7519ec2ad4deff60fa535480a03cd573
Kris Bredemeier <34@holbertonschool.com>
2016-05-10 20:32:51 -0700

    cat

---
+++ file_descriptors/3-cat/3-cat.c
@@ -0,0 +1,34 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+int main (int ac, char **argv)
+{
+  int fd;
+  char buf[BUFSIZ];
+
+  if (ac == 1)
+  {
+    while (read(0, &buf, 1))
+    {
+      write(1, &buf, 1);
+    }
+    return 0;
+  }
+
+  if (ac == 2)
+  {
+    fd = open(argv[1], O_RDONLY);
+    while (read(fd, &buf, 1))
+    {
+      write(1, &buf, 1);
+    }
+    if (close(fd))
+    {
+      return 1;
+    }
+    return 0;
+  }
+  return 1;
+}
