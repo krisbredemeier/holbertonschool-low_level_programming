c7aee670e8d1ca863d4b56da7f8fd96a024c38de
Kris Bredemeier <34@holbertonschool.com>
2016-07-24 22:50:52 -0700

    works on function that finds the depth of the binary tree

---
+++ trees/btree_depth.c
@@ -0,0 +1,51 @@
+#include "tree.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * btree_depth_r - function that find the depth of a binary tree- recursively
+ * @tree
+ *
+ * the depth of the tree must be returned by your function
+ * if the tree is empty, the depth is -1
+ */
+int btree_depth(BTree *tree);
+
+ int btree_depth_r(BTree *tree)
+ {
+   int left;
+   int right;
+
+   left = 1;
+   right = 1;
+   if (tree == NULL)
+   {
+     return 0;
+   }
+   left = left+btree_depth_r(tree->left);
+   right = right+btree_insert_r(tree->right);
+   if (left > right)
+   {
+     return left;
+   }
+   else
+   {
+     return right;
+   }
+ }
+
+ /**
+  * btree_depth - function that find the depth of a binary tree
+  * @tree
+  *
+  * the main body of the function
+  * but depends on recursive fucnction
+  */
+ int btree_depth(BTree *tree)
+ {
+   if (tree == NULL)
+   {
+     return -1;
+   }
+   return btree_depth_r(tree) -1;
+ }
