e2aaba69712ef4c759467f53cdd829266e2e59b4
Kris Bredemeier <34@holbertonschool.com>
2016-08-19 12:13:33 -0700

    beginning to build files based on alex examples

---
+++ raise_the_terrain/incline.c
@@ -0,0 +1,55 @@
+int init_instance(SDL_Instance *instance)
+{
+  /*Initalize SDL */
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+  {
+    fprintf(stderr, "unable to Initalize SDL: %s\n", SDL_GetError());
+    return (1);
+  }
+  /* create a new window instance */
+  instance->window = SDL_CreateWindow("SDL2 \\o/", SDL_WINDROWPOS_CENTERED),
+    SDL_WINDROWPOS_CENTERED, 1260, 720, 0);
+  if (instance->window == NULL)
+  {
+    fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+    SDL_Quit();
+    return (1);
+  }
+/* create a new Renderer instance linked to the window */
+instance->renderer = SDL_CreateRenderer(instance->window, -1,
+  SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+if (instance->renderer == NULL)
+  {
+    SDL_DestroyWindow(instance->window);
+    fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+    SDL_Quit();
+    return (1);
+  }
+return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // WX = incline * X - incline * Y;
+// // WY = (1 - incline) * X + (1 - incline) * Y - Z;
+// //
+//
+// #include <stdio.h>
+// #include <math.h>
+//
+//
+// int toIsometric2D(double x, double y,double z, double *u, double*v){
+//     *u=(x-z)/sqrt(2);
+//     *v=(x+2*y+z)/sqrt(6);
+//     return 0;
+// }
---
+++ raise_the_terrain/main.c
@@ -0,0 +1,87 @@
+#include <SDL2/SDL.h>
+
+int main(void)
+{
+  SDL_Instance instance;
+  if (init_instance(&instance) != 0)
+    return (1);
+  return (0);
+
+  while ("C is awesome")
+  {
+    SDL_SetRenderDrawColor(instance.renderer, 0, 0, 0, 0);
+    SDL_RenderClear(instance.renderer);
+    draw_stuff(instance);
+    SDL_RenderPresent(instance.renderer);
+  }
+  return (0);
+  // SDL_Window *window;
+  // SDL_Renderer *renderer;
+}
+
+void draw_stuff(SDL_Instance instance)
+{
+  SDL_SetRenderDrawColor(instance.renderer, 0xFF, 0xFF, 0xff, 0xFF);
+  SDL_RenderDrawLine(instance.renderer, 10, 10, 100, 100);
+}
+
+
+
+
+
+
+
+//
+// #include <stdio.h>
+// #include <math.h>
+//
+// int toIsometric2D(double x, double y,double z, double *u, double*v){
+//     *u=(x-z)/sqrt(2);
+//     *v=(x+2*y+z)/sqrt(6);
+//     return 0;
+// }
+//
+// int main(){
+//     int n=10;
+//     double z[n][n];
+//     int i,j;
+//
+//     for(i=0;i<9;i++){
+//         for(j=0;j<9;j++){
+//             z[i][j]=0;
+//         }
+//     }
+//
+//     for(i=2;i<8;i++){
+//         for(j=2;j<8;j++){
+//             z[i][j]=9;
+//         }
+//     }
+//
+//     for(i=4;i<6;i++){
+//         for(j=4;j<6;j++){
+//             z[i][j]=-5;
+//         }
+//     }
+//
+//     double u[n][n];
+//     double v[n][n];
+//
+//     for(i=0;i<9;i++){
+//         for(j=0;j<9;j++){
+//             toIsometric2D(i, j,z[i][j], &u[i][j],&v[i][j]);
+//         }
+//     }
+//
+//     //print to file -> gnuplot
+//
+//     FILE* fp;
+//     fp=fopen("data.dat","w");
+//     for(i=0;i<9;i++){
+//         for(j=0;j<9;j++){
+//             fprintf(fp,"%g %g %g\n",u[i][j],v[i][j],z[i][j]);
+//         }
+//     }
+//
+//     fclose(fp);
+// }
